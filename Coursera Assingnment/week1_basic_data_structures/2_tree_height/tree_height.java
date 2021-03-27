57136829ea6eca1736722de6034836ce711b7002
matthew <mematthew123@gmail.com>
2021-03-27 15:27:45 -0600

    committing completed assign 1 & 2

--- Coursera Assingnment/week1_basic_data_structures/2_tree_height/tree_height.java
+++ Coursera Assingnment/week1_basic_data_structures/2_tree_height/tree_height.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-
 public class tree_height {
     class FastScanner {
 		StringTokenizer tok = new StringTokenizer("");
@@ -22,20 +21,40 @@ public class tree_height {
 
 	public class TreeHeight {
 		int n;
-		int parent[];
+		//int parent[];
+		ArrayList<ArrayList<Integer>> children;
+		int root;
 		
 		void read() throws IOException {
 			FastScanner in = new FastScanner();
 			n = in.nextInt();
-			parent = new int[n];
+			children = new ArrayList<ArrayList<Integer>>(n);
+			for(int i = 0;i< n;i++)
+				children.add(new ArrayList<Integer>());
+			//parent = new int[n];
 			for (int i = 0; i < n; i++) {
-				parent[i] = in.nextInt();
+
+				int parent = in.nextInt();
+				if(parent == -1)
+					root = i;
+				else {
+
+					//System.out.println(children.get(parent).size());
+					children.get(parent).add(i);
+					//System.out.println("go");
+				}
 			}
 		}
-
+		int compute(int root){
+			int max = 0;
+			for (int i = 0;i < children.get(root).size();i++){
+				max = Math.max(max,compute(children.get(root).get(i)));
+			}
+			return max + 1;
+		}
 		int computeHeight() {
                         // Replace this code with a faster implementation
-			int maxHeight = 0;
+			/*int maxHeight = 0;
 			for (int vertex = 0; vertex < n; vertex++) {
 				int height = 0;
 				for (int i = vertex; i != -1; i = parent[i])
@@ -43,6 +62,8 @@ public class tree_height {
 				maxHeight = Math.max(maxHeight, height);
 			}
 			return maxHeight;
+			*/
+			return compute(root);
 		}
 	}
 
